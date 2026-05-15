c7f92b57049300781162cde92cd0e4b4ab2e6dcd
yacinos55 <abdouahmedyacine@gmail.com>
2026-05-15 15:12:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       __SB_URL__: JSON.stringify(env.VITE_SUPABASE_URL || "https://rsuvfbcpribrmkbveiyp.supabase.co"),
-      __SB_KEY__: JSON.stringify(env.VITE_SUPABASE_ANON_KEY || ""),
+      __SB_KEY__: JSON.stringify(env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzdXZmYmNwcmlicm1rYnZlaXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NzM0MjksImV4cCI6MjA5NDI0OTQyOX0.w76osoCdCN_Jkjge-fhZTHB7XUUM_vVTsFrt-CYCdfo"),
     },
   };
 });
