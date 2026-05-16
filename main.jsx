8a44738b2ee8b2260598bc0f4175953aa1aa622a
yacinos55 <abdouahmedyacine@gmail.com>
2026-05-16 19:42:14 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./GymOS_v3_FINAL.jsx";
 
 createRoot(document.getElementById("root")).render(<App />);
